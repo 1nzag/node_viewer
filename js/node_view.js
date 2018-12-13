15a31bc96ed4bdfa1921ea2e0f7a04018e305a66
1nzag <wkdwns420@gmail.com>
2018-12-13 04:49:19 +0000

    Add Tree Structure

--- js/node_view.js
+++ js/node_view.js
@@ -5,23 +5,55 @@ var NodeHeight = 40;
 var NodeColor = "#01DFD7";
 var NodeTextleft = 40;
 var NodeTextHeight = 20;
+var BaseTop = 200;
+var BaseLeft = 50;
 
 var NodeId = 0;
 
-function CreateRoot()
+var TreeRoot = new Object();
+
+/* node structure
+node id
+node name
+node child (list)
+*/
+
+function CreateNode(NodeName)
+{
+    var Node = new Object();
+    Node.NodeId = NodeId;
+    Node.NodeName = NodeName;
+    Node.ChildList = new Array();
+    return Node;
+}
+
+function DisplayNode(lNodeId, lNodename, Width, Height)
 {
     var canvas = document.getElementById("NodeCanvas");
     var context = canvas.getContext("2d");
     context.fillStyle = NodeColor;
-    context.fillRect(50,300,NodeWidth,NodeHeight);
+    context.fillRect(Width,Height,NodeWidth,NodeHeight);
     //draw rect
 
     context.fillStyle = "Black"
     context.font = "12px Arial"
     context.textAlign = "center";
     context.textBaseline = "middle";
-    context.fillText(String(NodeId) + ": Root" ,50 + NodeTextleft , 300 + NodeTextHeight);
+    context.fillText(String(lNodeId) + ": " + lNodename ,50 + NodeTextleft , 200 + NodeTextHeight);
     //fill text
 
+}
+
+function DrawTree()
+{
+    DisplayNode(TreeRoot.NodeId, TreeRoot.NodeName, 50, 200);
+    alert(TreeRoot.NodeName);
+}
+
+function CreateRoot()
+{
+        TreeRoot = CreateNode("Root");
+        NodeId = NodeId + 1; //50, 200
+        DrawTree();
 }
 
